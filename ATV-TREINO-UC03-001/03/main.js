47e20ed95a085c3428a57bdf74cd40f21b481079
atmam-danilo <waithmamdanilo@gmail.com>
2025-08-07 19:11:32 -0300

    Add files via upload

---
+++ ATV-TREINO-UC03-001/03/main.js
@@ -0,0 +1,8 @@
+let funcionario, vendas, comissao;
+
+funcionario=prompt(" Digite o nome do funcionário.")
+vendas=prompt(" Digite o total de vendas.")
+
+comissao=(10*vendas/100);
+
+comissao=alert(` A comissão de ${funcionario} é de ${comissao}`)
